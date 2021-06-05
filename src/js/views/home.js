9b8f9770da0ad89d02ad3132b0d3f455cf5ec7d4
Mauricio Lizcano <mauriciolizcano25531@gmail.com>
2021-06-04 23:06:06 -0400

    1er commit 7mo Hokage

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -5,6 +5,7 @@ import "../../styles/home.scss";
 export const Home = () => (
 	<div className="text-center mt-5">
 		<h1>Hello Rigo!</h1>
+		<h1>EL Wizard Hernandez</h1>
 		<p>
 			<img src={rigoImage} />
 		</p>
