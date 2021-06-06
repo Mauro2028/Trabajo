eb7e2b03616ebfda23a0796ac6372e28444077e0
Jose <jose@MacBook-Pro-de-Jose.local>
2021-06-06 00:26:43 -0400

    cambio footer 1.0

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import logo from "../../img/Logo.png";
+import Styles from "./footer.css";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p />
+		<div style={Styles.logoFooter} className="logoFooter">
+			<img src={logo} width="400px" height="80px" />
+		</div>
+		<div style={Styles}>
+			<p>© 2021 ERGTEC.</p>
+		</div>
 	</footer>
 );
