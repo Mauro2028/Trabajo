8d540488aa8c6d44c5eed4dc080a046e6cbc7793
Mauricio Lizcano <mauriciolizcano25531@gmail.com>
2021-06-06 00:22:34 -0400

    trabajado en navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../img/Logo.png";
+import mexico from "../../img/mexico.jpeg";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-dark " style={{ background: "#323336" }}>
 			<img src={logo} width="250px" height="80px" marginleft="5px" />
-			<h4>Inicio</h4>
-			<h4>Servicios</h4>
-			<h4>Acerca de </h4>
-			<h4>Proyectos</h4>
-			<h4>Contacto</h4>
+
+			<div className="form-group" style={{ display: "inline-flex" }}>
+				<h4 onClick={console.log("bailar")} style={{ color: "white", fontSize: "20px", margin: "20px" }}>
+					Inicio
+				</h4>
+				<h4 style={{ color: "white", fontSize: "20px", margin: "20px", marginRight: "10px" }}>Servicios</h4>
+				<h4 style={{ color: "white", fontSize: "20px", margin: "20px", marginRight: "10px" }}>Acerca de </h4>
+				<h4 style={{ color: "white", fontSize: "20px", margin: "20px", marginRight: "10px" }}>Proyectos</h4>
+				<h4 style={{ color: "white", fontSize: "20px", margin: "20px", marginRight: "10px" }}>Contacto</h4>
+				<select
+					style={{
+						fontSize: "20px",
+						margin: "20px",
+						marginRight: "30px",
+						color: "white",
+						background: "#323336",
+						border: "#323336"
+					}}>
+					{" "}
+					<option value="vacio">Idioma</option>
+					<option value="Espanol">Espanol</option>
+					<option value="Ingles">Ingles</option>
+				</select>
+			</div>
 		</nav>
 	);
 };
